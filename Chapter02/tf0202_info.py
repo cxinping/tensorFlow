2e230118ac66377d103dcc11a2cdfc567a7239fd
wangshuo <xpws2006@163.com>
2017-06-08 16:29:27 +0800

    打印子集信息

---
+++ Chapter02/tf0202_info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist  
+
+print( mnist.train.images.shape )
+print( mnist.train.labels.shape )
+print( mnist.validation.images.shape )
+print( mnist.validation.labels.shape )
+print( mnist.test.images.shape )
+print( mnist.test.labels.shape )
+
+
