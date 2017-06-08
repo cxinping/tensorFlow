1a33b6bf62b3947760cc4e8344a533dfdf57df14
wangshuo <xpws2006@163.com>
2017-06-08 17:35:32 +0800

    下载iris数据

---
+++ Chapter02/tf0203_csv.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+#import tensorflow.contrib.learn.python.learn.datasets.base as base
+from tensorflow.contrib.learn.python.learn.datasets import base
+
+
+#iris_data,iris_label = base.load_iris()
+#house_data,house_label= base.load_boston()
+
+base.load_iris() 
+
+
+
+
+
+
+
