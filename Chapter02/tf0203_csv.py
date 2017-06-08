ccb9144abbdc47c5a02f5ed72933471194e8797e
wangshuo <xpws2006@163.com>
2017-06-08 22:00:13 +0800

    数据集合下载

--- Chapter02/tf0203_csv.py
+++ Chapter02/tf0203_csv.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-#import tensorflow.contrib.learn.python.learn.datasets.base as base
 from tensorflow.contrib.learn.python.learn.datasets import base
 
+iris_data,iris_label = base.load_iris()
+house_data,house_label= base.load_boston()
 
-#iris_data,iris_label = base.load_iris()
-#house_data,house_label= base.load_boston()
-
-base.load_iris() 
 
 
 
