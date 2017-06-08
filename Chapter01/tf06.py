30afe791557953c5798411aa499886a7fbe2b7d6
wangshuo <xpws2006@163.com>
2017-06-08 16:02:52 +0800

    矩阵乘法

--- Chapter01/tf06.py
+++ Chapter01/tf06.py
@@ -2,11 +2,12 @@
 
 import tensorflow as tf
 
-a = tf.placeholder(tf.int16)
-b = tf.placeholder(tf.int16)
-add = tf.add(a, b)
-multiply = tf.multiply(a, b)
+a = tf.Variable(tf.ones([3,2]))
+b = tf.Variable(tf.ones([2,3]))
+#product = tf.matmul(5*a,4*b)
+product= tf.matmul(tf.multiply(5.0,a),tf.multiply(4.0,b))
+init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
-	print('a+b=' , sess.run(add, feed_dict={a: 2, b: 3}))
-	print('a*b=' , sess.run(multiply, feed_dict={a: 2, b: 3}))
+	sess.run(init)
+	print(sess.run(product))
